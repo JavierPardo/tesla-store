ce92958a6a7c4e664ff5a74f800a99ba0cc53944
Javier Pardo <javier.p.g1989@gmail.com>
2025-06-12 11:23:48 -0400

    Deploy Next.js build to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,9 @@ const nextConfig = {
   // Por ejemplo, si tu repo es 'your-username.github.io/my-dental-website'
   // basePath: '/my-dental-website',
   // assetPrefix: '/my-dental-website/',
+    basePath: process.env.NODE_ENV === 'production' ? '/tesla-store' : '', // Cambia 'tienda3d-catalogo' por el nombre de tu repositorio
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/tesla-store/' : '', // Asegúrate de que el prefijo del asset también sea correcto
+
 };
 
 export default nextConfig;
