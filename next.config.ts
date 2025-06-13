27feb586603e2ab88df2b0d6b76eab811932de0e
Javier Pardo <javier.p.g1989@gmail.com>
2025-06-13 17:01:30 -0400

    add to github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+
+  output: 'export', // This is the key line for static export
+  reactStrictMode: true,
+  // Optional: Add a base path if your GitHub Pages URL is like username.github.io/repo-name/
+  // If your repo is `my-store-repo` and your URL is `username.github.io/my-store-repo/`
+  // basePath: '/my-store-repo',
+  // images: {
+  //   unoptimized: true, // Required for static export if you use next/image
+  // },
 };
 
 export default nextConfig;
